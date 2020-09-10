1a5a7c7b52b8c10713627808df0d9a38c8bfa1c5
greensponge <greensponge@gmail.com>
2020-09-10 22:35:03 +0300

    add undo/redo, remove broken async tangle for now

--- init.el
+++ init.el
@@ -6,9 +6,7 @@
 (require 'package)
 (package-initialize)
 
-(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
-    (load-file (expand-file-name "config.el" user-emacs-directory))
-  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 ;; Make gc pause faster by decreasing the threshold.
 (setq gc-cons-threshold (* 10 1000 1000))
